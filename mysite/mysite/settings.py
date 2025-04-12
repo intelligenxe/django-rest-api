4a7663e7033d0de5ca166463c13f701e25aae8e0
intelligenxe <drivas@intelligenxe.com>
2025-04-12 10:42:49 -0600

    first changes from original

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -77,14 +77,22 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": os.getenv("MARIADB_DATABASE"),
+#         "USER": os.getenv("MARIADB_USER"),
+#         "PASSWORD": os.getenv("MARIADB_ROOT_PASSWORD"),
+#         "HOST": os.getenv("MARIADB_HOST"),
+#         "PORT": os.getenv("MARIADB_PORT", 3306),
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.getenv("MARIADB_DATABASE"),
-        "USER": os.getenv("MARIADB_USER"),
-        "PASSWORD": os.getenv("MARIADB_ROOT_PASSWORD"),
-        "HOST": os.getenv("MARIADB_HOST"),
-        "PORT": os.getenv("MARIADB_PORT", 3306),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
